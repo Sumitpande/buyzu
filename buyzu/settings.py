be7555b027a0a40066be615c31ddae3568606338
Sumitpande <50658237+Sumitpande@users.noreply.github.com>
2021-01-14 14:59:39 +0530

    Update settings.py

--- buyzu/settings.py
+++ buyzu/settings.py
@@ -190,7 +190,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'sumitakashpande1230@gmail.com'
-EMAIL_HOST_PASSWORD = 'sumit123'
+EMAIL_HOST_PASSWORD = '*******'
 
 REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
